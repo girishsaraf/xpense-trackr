08215a851d2aab7f0e66e859860fc3d4574432a4
Girish Saraf <girishsaraf03@gmail.com>
2024-03-30 09:48:51 -0700

    Changes in main flo

--- backend/cmd/expense-tracker/main.go
+++ backend/cmd/expense-tracker/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"github.com/girishsaraf/xpense-trackr/backend/config"
+	"github.com/girishsaraf/xpense-trackr/backend/internal"
+	"github.com/girishsaraf/xpense-trackr/backend/internal/budget"
+	"github.com/girishsaraf/xpense-trackr/backend/internal/category"
+	"github.com/girishsaraf/xpense-trackr/backend/internal/expense"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
 
+	// Setting Environment Variables - Will be replaced with Dockerfile changes in the future
+	if err := os.Setenv("DB_HOST", "127.0.0.1"); err != nil {
+		// Handle error
+		log.Fatalf("Error setting DB_HOST environment variable: %v", err)
+	}
+	if err := os.Setenv("DB_USER", "root"); err != nil {
+		// Handle error
+		log.Fatalf("Error setting DB_USER environment variable: %v", err)
+	}
+	if err := os.Setenv("DB_PASS", "root_password"); err != nil {
+		// Handle error
+		log.Fatalf("Error setting DB_PASS environment variable: %v", err)
+	}
+	if err := os.Setenv("DB_NAME", "xpense_trackr_db"); err != nil {
+		// Handle error
+		log.Fatalf("Error setting DB_NAME environment variable: %v", err)
+	}
+	if err := os.Setenv("DB_PORT", "3306"); err != nil {
+		// Handle error
+		log.Fatalf("Error setting DB_PORT environment variable: %v", err)
+	}
+
+	// Setting Database Connections for all models
+	db := config.GetDatabaseConnectionObject(config.LoadDatabaseConfig())
+	budget.SetDB(db)
+	expense.SetDB(db)
+	category.SetDB(db)
+
+	// Creating API routes and starting server on 8080
+	router := mux.NewRouter()
+	internal.SetRoutes(router)
+	log.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
