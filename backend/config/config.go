e71538e84bb1e037c3277a33b61dd310e677c2ad
Girish Saraf <girishsaraf03@gmail.com>
2024-03-30 09:42:06 -0700

    Added DB Config

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+// LoadDatabaseConfig loads the database configuration from environment variables
+func LoadDatabaseConfig() DatabaseConfig {
+	//return DatabaseConfig{
+	//	Host:     "127.0.0.1",
+	//	Port:     "3306",
+	//	User:     "root",
+	//	Password: "root_password",
+	//	Name:     "xpense_trackr_db",
+	//}
+	return DatabaseConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+}
+
+func GetDatabaseConnectionObject(dbConfig DatabaseConfig) *gorm.DB {
+	// Formulate DSN from the configuration
+	dsn := dbConfig.User + ":" + dbConfig.Password + "@tcp(" + dbConfig.Host + ":" + dbConfig.Port + ")/" + dbConfig.Name + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	return db
+}
+
+// DatabaseConfig represents the database configuration
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
