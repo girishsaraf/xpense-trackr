2fb0b1b0c15b0dc30dc1fb4569672aa3593e5662
Girish Saraf <girishsaraf03@gmail.com>
2024-03-31 19:33:07 -0700

    Added pagination component

---
+++ frontend/xpense-trackr-frontend/src/components/Pagination/Pagination.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Pagination.css';
+
+const Pagination = ({ currentPage, itemsPerPage, totalItems, paginate }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className={currentPage === number ? 'active' : ''}>
+                        <button onClick={() => paginate(number)} className="page-link">
+                            {number}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
