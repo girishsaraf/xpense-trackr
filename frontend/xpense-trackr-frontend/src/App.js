c542058e55d19e64dceb67d7e61ac9091ff06e1a
Girish Saraf <girishsaraf03@gmail.com>
2024-04-01 19:26:15 -0700

    Main app UI changes

--- frontend/xpense-trackr-frontend/src/App.js
+++ frontend/xpense-trackr-frontend/src/App.js
@@ -5,23 +5,28 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import ExpenseList from './components/Expense/ExpenseList';
 import './App.css';
 import ExpenseForm from "./components/Expense/ExpenseForm";
+import InvestmentList from "./components/Investment/InvestmentList";
+import InvestmentForm from "./components/Investment/InvestmentForm"
 
 function App() {
     return (
         <Router>
-            <div>
+            <div className="xpense-trackr-main">
                 <h1>Xpense-Trackr</h1>
                 <nav>
                     <ul className="horizontal-tabs">
                         <li><NavLink to="/expenses" activeClassName="active-tab">Expense</NavLink></li>
-                        <li><NavLink to="/categories" activeClassName="active-tab">Category</NavLink></li>
-                        <li><NavLink to="/budgets" activeClassName="active-tab">Budget</NavLink></li>
+                        <li><NavLink to="/investments" activeClassName="active-tab">Investments</NavLink></li>
+                        <li><NavLink to="/budgets" activeClassName="active-tab">Plan your Budget</NavLink></li>
+                        <li><NavLink to="/goals" activeClassName="active-tab">Investment Goals</NavLink></li>
                         <li><NavLink to="/trends" activeClassName="active-tab">Trends</NavLink></li>
                     </ul>
                 </nav>
                 <Routes>
                     <Route path="/expenses" element={<ExpenseList />} />
                     <Route path="/expenses/new" element={<ExpenseForm />} />
+                    <Route path="/investments" element={<InvestmentList />} />
+                    <Route path="/investments/new" element={<InvestmentForm />} />
                     {/*<Route path="/categories" element={<CategoryList />} />*/}
                     {/*<Route path="/budgets" element={<BudgetList />} />*/}
                     {/*<Route path="/trends" element={<TrendsPage />} />*/}
