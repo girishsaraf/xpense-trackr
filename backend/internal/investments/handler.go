e04f06c8b0dc000e0c16dfcf25212f46c1bf9103
Girish Saraf <girishsaraf03@gmail.com>
2024-04-01 19:24:35 -0700

    Added investments table handler and model

--- backend/internal/investments/handler.go
+++ backend/internal/investments/handler.go
@@ -1,6 +1,6 @@
-// internal/budget/handler.go
+// internal/Investment/handler.go
 
-package budget
+package investments
 
 import (
 	"encoding/json"
@@ -9,73 +9,73 @@ import (
 	"strconv"
 )
 
-// GetAllBudgetsHandler handles the request to retrieve all budgets.
-func GetAllBudgetsHandler(w http.ResponseWriter, r *http.Request) {
-	budgets, err := GetAllBudgets()
+// GetAllInvestmentsHandler handles the request to retrieve all Investments.
+func GetAllInvestmentsHandler(w http.ResponseWriter, r *http.Request) {
+	Investments, err := GetAllInvestments()
 	if err != nil {
-		http.Error(w, "Failed to fetch budgets", http.StatusInternalServerError)
+		http.Error(w, "Failed to fetch Investments", http.StatusInternalServerError)
 		return
 	}
-	respondWithJSON(w, budgets)
+	respondWithJSON(w, Investments)
 }
 
-// CreateBudgetHandler handles the request to create a new budget.
-func CreateBudgetHandler(w http.ResponseWriter, r *http.Request) {
-	var budget Budget
-	if err := json.NewDecoder(r.Body).Decode(&budget); err != nil {
+// CreateInvestmentHandler handles the request to create a new Investment.
+func CreateInvestmentHandler(w http.ResponseWriter, r *http.Request) {
+	var Investment Investment
+	if err := json.NewDecoder(r.Body).Decode(&Investment); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-	if err := CreateBudget(&budget); err != nil {
-		http.Error(w, "Failed to create budget", http.StatusInternalServerError)
+	if err := CreateInvestment(&Investment); err != nil {
+		http.Error(w, "Failed to create Investment", http.StatusInternalServerError)
 		return
 	}
-	respondWithJSON(w, budget)
+	respondWithJSON(w, Investment)
 }
 
-// GetBudgetByIDHandler handles the request to retrieve a specific budget by ID.
-func GetBudgetByIDHandler(w http.ResponseWriter, r *http.Request) {
-	budgetID, err := strconv.Atoi(mux.Vars(r)["id"])
+// GetInvestmentByIDHandler handles the request to retrieve a specific Investment by ID.
+func GetInvestmentByIDHandler(w http.ResponseWriter, r *http.Request) {
+	InvestmentID, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		http.Error(w, "Invalid budget ID", http.StatusBadRequest)
+		http.Error(w, "Invalid Investment ID", http.StatusBadRequest)
 		return
 	}
-	budget, err := GetBudgetByID(budgetID)
+	Investment, err := GetInvestmentByID(InvestmentID)
 	if err != nil {
-		http.Error(w, "Budget not found", http.StatusNotFound)
+		http.Error(w, "Investment not found", http.StatusNotFound)
 		return
 	}
-	respondWithJSON(w, budget)
+	respondWithJSON(w, Investment)
 }
 
-// UpdateBudgetHandler handles the request to update an existing budget.
-func UpdateBudgetHandler(w http.ResponseWriter, r *http.Request) {
-	budgetID, err := strconv.Atoi(mux.Vars(r)["id"])
+// UpdateInvestmentHandler handles the request to update an existing Investment.
+func UpdateInvestmentHandler(w http.ResponseWriter, r *http.Request) {
+	InvestmentID, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		http.Error(w, "Invalid budget ID", http.StatusBadRequest)
+		http.Error(w, "Invalid Investment ID", http.StatusBadRequest)
 		return
 	}
-	var updatedBudget Budget
-	if err := json.NewDecoder(r.Body).Decode(&updatedBudget); err != nil {
+	var updatedInvestment Investment
+	if err := json.NewDecoder(r.Body).Decode(&updatedInvestment); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-	if err := UpdateBudget(budgetID, &updatedBudget); err != nil {
-		http.Error(w, "Failed to update budget", http.StatusInternalServerError)
+	if err := UpdateInvestment(InvestmentID, &updatedInvestment); err != nil {
+		http.Error(w, "Failed to update Investment", http.StatusInternalServerError)
 		return
 	}
-	respondWithJSON(w, updatedBudget)
+	respondWithJSON(w, updatedInvestment)
 }
 
-// DeleteBudgetHandler handles the request to delete an existing budget.
-func DeleteBudgetHandler(w http.ResponseWriter, r *http.Request) {
-	budgetID, err := strconv.Atoi(mux.Vars(r)["id"])
+// DeleteInvestmentHandler handles the request to delete an existing Investment.
+func DeleteInvestmentHandler(w http.ResponseWriter, r *http.Request) {
+	InvestmentID, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
-		http.Error(w, "Invalid budget ID", http.StatusBadRequest)
+		http.Error(w, "Invalid Investment ID", http.StatusBadRequest)
 		return
 	}
-	if err := DeleteBudget(budgetID); err != nil {
-		http.Error(w, "Failed to delete budget", http.StatusInternalServerError)
+	if err := DeleteInvestment(InvestmentID); err != nil {
+		http.Error(w, "Failed to delete Investment", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
--- backend/internal/investments/model.go
+++ backend/internal/investments/model.go
@@ -1,6 +1,6 @@
-// internal/model/budget.go
+// internal/model/Investment.go
 
-package budget
+package investments
 
 import (
 	"errors"
@@ -9,7 +9,7 @@ import (
 
 var db *gorm.DB
 
-// SetDB sets the db object for the budget package.
+// SetDB sets the db object for the Investment package.
 func SetDB(databaseConfig *gorm.DB) {
 	db = databaseConfig
 }
@@ -17,46 +17,46 @@ func SetDB(databaseConfig *gorm.DB) {
 // ErrRecordNotFound is an error indicating that a record was not found.
 var ErrRecordNotFound = errors.New("record not found")
 
-// Budget represents the budget model.
-type Budget struct {
-	ID         uint    `gorm:"primaryKey" json:"id"`
-	UserID     uint    `json:"user_id"`
-	CategoryID uint    `json:"category_id"`
-	Amount     float64 `json:"amount"`
+// Investment represents the Investment model.
+type Investment struct {
+	ID             uint    `gorm:"primaryKey" json:"id"`
+	Amount         float64 `json:"amount,string,omitempty"`
+	InvestmentType string  `json:"investment_type"`
+	Date           string  `json:"date"`
 }
 
-// GetAllBudgets retrieves all budgets from the database.
-func GetAllBudgets() ([]Budget, error) {
-	var budgets []Budget
-	if err := db.Find(&budgets).Error; err != nil {
+// GetAllInvestments retrieves all Investments from the database.
+func GetAllInvestments() ([]Investment, error) {
+	var Investments []Investment
+	if err := db.Find(&Investments).Error; err != nil {
 		return nil, err
 	}
-	return budgets, nil
+	return Investments, nil
 }
 
-// GetBudgetByID retrieves a specific budget by its ID from the database.
-func GetBudgetByID(id int) (*Budget, error) {
-	var budget Budget
-	if err := db.First(&budget, id).Error; err != nil {
+// GetInvestmentByID retrieves a specific Investment by its ID from the database.
+func GetInvestmentByID(id int) (*Investment, error) {
+	var Investment Investment
+	if err := db.First(&Investment, id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, ErrRecordNotFound
 		}
 		return nil, err
 	}
-	return &budget, nil
+	return &Investment, nil
 }
 
-// CreateBudget creates a new budget in the database.
-func CreateBudget(budget *Budget) error {
-	if err := db.Create(&budget).Error; err != nil {
+// CreateInvestment creates a new Investment in the database.
+func CreateInvestment(Investment *Investment) error {
+	if err := db.Create(&Investment).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-// UpdateBudget updates an existing budget in the database.
-func UpdateBudget(id int, updatedBudget *Budget) error {
-	if err := db.Model(&Budget{}).Where("id = ?", id).Updates(updatedBudget).Error; err != nil {
+// UpdateInvestment updates an existing Investment in the database.
+func UpdateInvestment(id int, updatedInvestment *Investment) error {
+	if err := db.Model(&Investment{}).Where("id = ?", id).Updates(updatedInvestment).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return ErrRecordNotFound
 		}
@@ -65,9 +65,9 @@ func UpdateBudget(id int, updatedBudget *Budget) error {
 	return nil
 }
 
-// DeleteBudget deletes an existing budget from the database.
-func DeleteBudget(id int) error {
-	if err := db.Delete(&Budget{}, id).Error; err != nil {
+// DeleteInvestment deletes an existing Investment from the database.
+func DeleteInvestment(id int) error {
+	if err := db.Delete(&Investment{}, id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return ErrRecordNotFound
 		}
