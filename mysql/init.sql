1fba2d2fb3858eef6ec48f226a3462bd2c06aafa
Girish Saraf <girishsaraf03@gmail.com>
2024-04-06 22:28:53 -0700

    Added relative date in mysql queries

--- mysql/init.sql
+++ mysql/init.sql
@@ -106,48 +106,49 @@ INSERT INTO categories (name) VALUES
 
 -- Insert query for 'Food'
 INSERT INTO expenses (category_id, amount, description, date) VALUES
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Groceries', '2024-03-01'),
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Dining out', '2024-02-01'),
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Food delivery', '2024-01-01'),
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Home cooking', '2023-12-01'),
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Snacks', '2023-11-01'),
-      (1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Restaurant', '2023-10-01');
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Groceries', DATE_SUB(CURRENT_DATE(), INTERVAL 0 MONTH)),
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Dining out', DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)),
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Food delivery', DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)),
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Home cooking', DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH)),
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Snacks', DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)),
+(1, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Restaurant', DATE_SUB(CURRENT_DATE(), INTERVAL 5 MONTH));
 
 -- Insert query for 'Transportation'
 INSERT INTO expenses (category_id, amount, description, date) VALUES
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gasoline', '2024-03-01'),
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Public transportation', '2024-02-01'),
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Taxi', '2024-01-01'),
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Ride-sharing', '2023-12-01'),
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Car maintenance', '2023-11-01'),
-      (2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Parking fees', '2023-10-01');
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gasoline', DATE_SUB(CURRENT_DATE(), INTERVAL 0 MONTH)),
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Public transportation', DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)),
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Taxi', DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)),
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Ride-sharing', DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH)),
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Car maintenance', DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)),
+(2, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Parking fees', DATE_SUB(CURRENT_DATE(), INTERVAL 5 MONTH));
 
 -- Insert query for 'Utilities'
 INSERT INTO expenses (category_id, amount, description, date) VALUES
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Electricity', '2024-03-01'),
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Water', '2024-02-01'),
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Internet', '2024-01-01'),
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Phone bill', '2023-12-01'),
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gas', '2023-11-01'),
-      (3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Trash collection', '2023-10-01');
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Electricity', DATE_SUB(CURRENT_DATE(), INTERVAL 0 MONTH)),
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Water', DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)),
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Internet', DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)),
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Phone bill', DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH)),
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gas', DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)),
+(3, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Trash collection', DATE_SUB(CURRENT_DATE(), INTERVAL 5 MONTH));
 
 -- Insert query for 'Entertainment'
 INSERT INTO expenses (category_id, amount, description, date) VALUES
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Movie tickets', '2024-03-01'),
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Concert', '2024-02-01'),
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Video games', '2024-01-01'),
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Books', '2023-12-01'),
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Streaming service', '2023-11-01'),
-      (4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Board games', '2023-10-01');
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Movie tickets', DATE_SUB(CURRENT_DATE(), INTERVAL 0 MONTH)),
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Concert', DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)),
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Video games', DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)),
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Books', DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH)),
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Streaming service', DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)),
+(4, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Board games', DATE_SUB(CURRENT_DATE(), INTERVAL 5 MONTH));
 
 -- Insert query for 'Healthcare'
 INSERT INTO expenses (category_id, amount, description, date) VALUES
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Medications', '2024-03-01'),
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Doctor visit', '2024-02-01'),
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Health insurance', '2024-01-01'),
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Dental care', '2023-12-01'),
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gym membership', '2023-11-01'),
-      (5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Prescription glasses', '2023-10-01');
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Medications', DATE_SUB(CURRENT_DATE(), INTERVAL 0 MONTH)),
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Doctor visit', DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)),
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Health insurance', DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)),
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Dental care', DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH)),
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Gym membership', DATE_SUB(CURRENT_DATE(), INTERVAL 4 MONTH)),
+(5, FLOOR(RAND() * (250 - 10 + 1)) + 10, 'Prescription glasses', DATE_SUB(CURRENT_DATE(), INTERVAL 5 MONTH));
+
 
 
 INSERT INTO budgets (category_id, amount) VALUES
@@ -182,15 +183,16 @@ CREATE TABLE recurring_investment (
 
 INSERT INTO recurring_expense (description, amount, frequency, start_date, end_date, category_id)
 VALUES
-    ('Internet Subscription', 49.99, 'monthly', '2024-04-01', NULL, 1),
-    ('Gym Membership', 29.99, 'monthly', '2024-04-01', NULL, 2),
-    ('Mobile Phone Plan', 59.99, 'monthly', '2024-04-01', NULL, 3),
-    ('Scoop to Work', 6.99, 'daily', '2024-04-01', NULL, 3);
+    ('Internet Subscription', 49.99, 'monthly', CURRENT_DATE(), NULL, 1),
+    ('Gym Membership', 29.99, 'monthly', CURRENT_DATE(), NULL, 2),
+    ('Mobile Phone Plan', 59.99, 'monthly', CURRENT_DATE(), NULL, 3),
+    ('Scoop to Work', 6.99, 'daily', CURRENT_DATE(), NULL, 3);
 
 INSERT INTO recurring_investment (description, amount, frequency, start_date, end_date, type)
 VALUES
-    ('401(k) Contribution', 500.00, 'monthly', '2024-04-01', NULL, '401k'),
-    ('Mutual Funds Investment', 1000.00, 'monthly', '2024-04-01', NULL, 'mutual_funds'),
-    ('Stocks Investment', 1500.00, 'monthly', '2024-04-01', NULL, 'stocks');
+    ('401(k) Contribution', 500.00, 'monthly', CURRENT_DATE(), NULL, '401k'),
+    ('Mutual Funds Investment', 1000.00, 'monthly', CURRENT_DATE(), NULL, 'mutual_funds'),
+    ('Stocks Investment', 1500.00, 'monthly', CURRENT_DATE(), NULL, 'stocks');
+
 
 
