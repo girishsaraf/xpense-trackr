e4ad537a51a03e334311389f359e4034991af7ac
Girish Saraf <girishsaraf03@gmail.com>
2024-04-07 17:01:47 -0700

    Changes to remove expense API call

--- frontend/xpense-trackr-frontend/src/components/Expense/ExpenseForm.js
+++ frontend/xpense-trackr-frontend/src/components/Expense/ExpenseForm.js
@@ -10,21 +10,32 @@ import { useNavigate } from 'react-router-dom';
 const ExpenseForm = () => {
 
     const navigate = useNavigate();
-
     const [formData, setFormData] = useState({
         description: '',
         amount: '',
         date: '',
         category_id: '',
-        user_id: '',
+        recurring: false,
+        start_date: '',
+        end_date: '',
+        frequency: '',
     });
 
     const handleSubmit = async event => {
         event.preventDefault();
 
-        try {
-            await axios.post(`${backendUrl}/api/expenses`, formData);
-            alert('Expense added successfully');
+        let isRecurring = formData.recurring;
+
+        try
+        {
+            if(isRecurring){
+                await axios.post(`${backendUrl}/api/recurring/expenses`, formData);
+                alert('Expense added successfully');
+            }
+            else {
+                await axios.post(`${backendUrl}/api/expenses`, formData);
+                alert('Expense added successfully');
+            }
             clearFormData();
 
             navigate('/expenses');
@@ -40,15 +51,21 @@ const ExpenseForm = () => {
             amount: '',
             date: '',
             category_id: '',
-            user_id: '',
+            recurring: false,
+            start_date: '',
+            end_date: '',
+            frequency: '',
         });
         navigate('/expenses');
     }
 
     const handleInputChange = event => {
+        const { name, value, type, checked } = event.target;
+        const newValue = type === 'checkbox' ? checked : value;
+
         setFormData({
             ...formData,
-            [event.target.name]: event.target.value,
+            [name]: newValue,
         });
     };
 
@@ -56,16 +73,6 @@ const ExpenseForm = () => {
         <div className="expense-form-container">
             <h2>Add Expense</h2>
             <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <label htmlFor="user_id">User:</label>
-                    <input
-                        type="number"
-                        id="user_id"
-                        name="user_id"
-                        value={formData.user_id}
-                        onChange={handleInputChange}
-                    />
-                </div>
                 <div className="input-container">
                     <label htmlFor="description">Description:</label>
                     <input
@@ -106,6 +113,56 @@ const ExpenseForm = () => {
                         onChange={handleInputChange}
                     />
                 </div>
+                <div className="input-container">
+                    <label htmlFor="recurring">Is Recurring Expense?</label>
+                    <input
+                        type="checkbox"
+                        id="recurring"
+                        name="recurring"
+                        value={formData.recurring}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                {formData.recurring && (
+                    <>
+                        <div className="input-container">
+                            <label htmlFor="startDate">Start Date:</label>
+                            <input
+                                type="date"
+                                id="startDate"
+                                name="startDate"
+                                value={formData.start_date}
+                                defaultValue={formData.date}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="input-container">
+                            <label htmlFor="endDate">End Date:</label>
+                            <input
+                                type="date"
+                                id="endDate"
+                                name="endDate"
+                                value={formData.end_date}
+                                defaultValue={formData.date}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="input-container">
+                            <label htmlFor="frequency">Frequency:</label>
+                            <select
+                                id="frequency"
+                                name="frequency"
+                                value={formData.frequency}
+                                onChange={handleInputChange}
+                            >
+                                <option value="daily">Daily</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="biweekly">Bi-Weekly</option>
+                                <option value="monthly">Monthly</option>
+                            </select>
+                        </div>
+                    </>
+                )}
                 <div className="buttons-container">
                     <button type="submit">Add</button>
                     <button type="button" onClick={clearFormData}>Cancel</button>
